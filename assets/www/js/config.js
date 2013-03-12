82489cef5b986af15a12443ef75842c2ebd9742e
Lim Chee Kin <limcheekin@vobject.com>
2013-03-12 10:18:12 +0800

    switch off jqm transition for Android devices.

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -14,4 +14,13 @@ $(document).bind("mobileinit", function() {
 	$('div[data-role="page"]').on('pagehide', function(event, ui) {
 		$(event.currentTarget).remove();
 	});
+	
+	// switch off jqm transition for Android devices
+	// slide 46 of http://www.slideshare.net/AgentK/real-world-lessons-in-jquery-mobile
+	// Device detection: http://www.jquery4u.com/mobile/detect-mobile-devices-jquery/
+	var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
+
+	if (isAndroid)  {
+		$.mobile.defaultPageTransition = none;
+	}	
 });
